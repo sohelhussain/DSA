0b6f3fe8286bebb1c010756b1ec77c85793a3813
sohelhussain <sohelhussaing@gmail.com>
2024-06-12 23:19:10 +0530

    add the another quetions in operators

--- basics/Operators.java
+++ basics/Operators.java
@@ -36,10 +36,14 @@ public class Operators {
         // 11 + 13;
 
 
-        //✅ 1:-
+        // 2:-
 
-        // int i = 11;
-        // i = i-- + --i;    
-        // System.out.println(i);
+        // int a=11, b=22, c;
+         
+        // c = a + b + a++ + b++ + ++a + ++b;
+         
+        // System.out.println("a="+a);
+        // System.out.println("b="+b);
+        // System.out.println("c="+c);
     }
 }
