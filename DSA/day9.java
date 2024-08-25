ad0ac530e07aeed7734d719364d2492ff16df39c
sohelhussain <140825779+sohelhussain@users.noreply.github.com>
2024-08-25 23:48:20 +0530

    Update day9.java

--- DSA/day9.java
+++ DSA/day9.java
@@ -22,6 +22,8 @@ public class day9 {
             
     }
 
+    // ! Product of Array Except Self
+
     public static void main(String[] args) {
         int[] nums = { 1, 7, 3, 6, 5, 6 };
         System.out.println(Pivot(nums));
