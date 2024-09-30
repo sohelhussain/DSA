84a40a898d955404f27cdc7222446871fda9458e
sohelhussain <sohelhussaing@gmail.com>
2024-09-30 22:31:25 +0530

    feat: ADDING A DAY33 TOWEROFHANOI

--- DSA/day33.java
+++ DSA/day33.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class day33 {
+
+    //!  Tower of Hanoi
+
+    public static void towerOfHanoi(int n, int s, int h, int d){
+        if(n==1){
+            System.out.println(s + ' -> ' + d);
+            return
+        }
+        towerOfHanoi(n-1, s, d, h);
+        System.out.println(s + ' -> ' + d);
+        towerOfHanoi(n-1, h, s, d);
+    }
+
+
+    //! power set
+
     public static void powerKaSet(String s, int i, String cur, List<String> ans) {
         if(i == s.length()) {
             if(cur.length() > 0)
@@ -29,15 +45,28 @@ public class day33 {
         powerKaSetArrayWala(arr, i+1, cur, ans);
     }
     public static void main(String[] args) {
+
+        //! Tower of Hanoi
+
+        int n = 4;
+        int s = 1, h = 2, d = 3;
+        towerOfHanoi(n, s, h, d);
+
+
+        //! power ka set for string 
+
 //        String s = "abc";
 //        List<String> ans = new ArrayList<>();
 //        powerKaSet(s, 0, "", ans);
 //        Collections.sort(ans);
 //        System.out.println(ans);
-        int arr[] = {1,2,3};
-        List<Integer> cur = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        powerKaSetArrayWala(arr, 0, cur, ans);
-        System.out.println(ans);
+
+       //! power ka set for int array
+
+        // int arr[] = {1,2,3};
+        // List<Integer> cur = new ArrayList<>();
+        // List<List<Integer>> ans = new ArrayList<>();
+        // powerKaSetArrayWala(arr, 0, cur, ans);
+        // System.out.println(ans);
     }
 }
