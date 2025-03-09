da10c1579d096da267f3749eda252617793703d0
sohelhussain <140825779+sohelhussain@users.noreply.github.com>
2025-03-10 02:48:26 +0530

    Update day9.java

--- DSA/day9.java
+++ DSA/day9.java
@@ -24,6 +24,33 @@ public class day9 {
 
     // ! Product of Array Except Self
 
+    public static void Product(int[] nums){
+        int[] nums = { 1, 2, 3, 4 };
+        // int[] left = {1,1,2,6};
+        int[] left = new int[nums.length];
+
+        left[0] = 1;
+        for (int i = 1; i < nums.length ; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+
+        int[] ans = new int[nums.length];
+
+        for (int i = left.length - 1; i >= 0; i--) {
+            ans[i] = right * left[i];
+            right *= nums[i];
+        }
+
+        for(int i: ans){
+            System.err.println(i);
+        }
+        for(int i: left){
+            System.err.println(i);
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = { 1, 7, 3, 6, 5, 6 };
         System.out.println(Pivot(nums));
