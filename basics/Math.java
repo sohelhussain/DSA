77f7964fa538854620601db1cd11976bc947d804
sohelhussain <140825779+sohelhussain@users.noreply.github.com>
2025-05-30 10:04:10 +0530

    Update Math.java

--- basics/Math.java
+++ basics/Math.java
@@ -1,7 +1,7 @@
 public class Math {
 
   public static void perfectSqur(String[] args) {
-  // any value that properly divede by 100 that is called perfect Sqre = 100 % 2 = 0 
+  // find factorial any value that properly divide by 100 that is called perfect Square = 100 % 2 = 0 
        int n = 100;
        List<Integer> list = new ArrayList<>();
        for(int i = 1; i <= Math.sqrt(n); i++){
@@ -39,4 +39,28 @@ public class Math {
         }
         return true;
     }
+
+  // countPrime with optimal aproch
+   public static void countPrime(int n){
+
+    boolean prime[] = new boolean[n+1];
+    Arrays.fill(prime,true);
+    prime[0] = prime[1] = false;
+
+    for(int i = 2; i * i <= n; i++){
+        if (prime[i]) {
+            for(int j = i *i; j <= n; j = j+i){
+                prime[j] = false;
+            }
+        }
+    }
+
+    for(int i = 0; i <= n; i++){
+        if (prime[i]) {
+            System.out.println(i);
+        }
+    }
+
+   }
+
 }
