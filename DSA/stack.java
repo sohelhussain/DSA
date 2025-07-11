204a69eb3eed14cf4a84be7211e0dfe96f7f1290
sohelhussain <140825779+sohelhussain@users.noreply.github.com>
2025-07-11 23:23:34 +0530

    Create stack.java

---
+++ DSA/stack.java
@@ -0,0 +1,43 @@
+// 232. Implement Queue using Stacks
+class MyQueue {
+    Stack<Integer> st1, st2;
+
+    public MyQueue() {
+        st1 = new Stack<>();
+        st2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        st1.push(x);
+    }
+    
+    public int pop() {
+        while(!st1.isEmpty()) {
+            st2.push(st1.pop());
+        }
+        int ans = st2.pop();
+
+        while(!st2.isEmpty()) {
+            st1.push(st2.pop());
+        }
+        return ans;
+    }
+    
+    public int peek() {
+        while(!st1.isEmpty()) {
+            st2.push(st1.pop());
+        }
+
+        int ans = st2.peek();
+
+        while(!st2.isEmpty()) {
+            st1.push(st2.pop());
+        }
+        return ans;
+    }
+    
+    public boolean empty() {
+        return st1.isEmpty() ? true : false;
+    }
+}
+
