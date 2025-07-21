45149d17333715ca180305146ed45d17749de528
sohelhussain <140825779+sohelhussain@users.noreply.github.com>
2025-07-21 19:56:36 +0530

    Update TwoPointer.java

--- DSA/TwoPointer.java
+++ DSA/TwoPointer.java
@@ -86,6 +86,46 @@ class Solution {
 
 
 
+  // 18. 4Sum
+
+
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+       Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = j + 1, l = n - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+
+                        // Skip duplicates
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+
+        return list;
+    }
+
+
 
 
   
